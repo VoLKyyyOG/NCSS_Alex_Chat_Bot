4e60053cba5a6901d70cc077e00ec4f003c2aef2
Akira Wang <30443254+akiratwang@users.noreply.github.com>
2020-01-06 14:11:01 +1100

    Update test.py

--- test.py
+++ test.py
@@ -4,49 +4,6 @@ from appid import *
 
 app = Flask(__name__)
 
-@app.route('/ack', methods=['GET', 'POST'])
-def ack():
-  # Create a message to send to the server
-  message = {
-	'author': 'ACK',
-	'text': f'I got your message',
-  }
-
-  # Return the JSON
-  return jsonify(message)
-
-@app.route('/echo', methods=['POST'])
-def echo():
-  # Read the message from NeCSuS
-  data = request.get_json()
-  text = data.get('text').split()[1:]
-
-  # Create a message to send to the server
-  message = {
-    'author': 'ECHO',
-    'text': f'You said: {" ".join(text)}',
-  }
-
-  # Return the JSON
-  return jsonify(message)
-
-@app.route('/roll', methods=['POST'])
-def roll():
-  # Get the message data from NeCSuS
-  data = request.get_json()
-  author = data.get('author', 'Someone')
-  command = data.get('text', '')
-  sides = int(command.split()[-1])
-
-  # Reply to the message
-  message = {
-    'author': 'GAMBLE',
-    'text': f'{author} rolled a {sides} sided dice and got a {random.randint(1, sides)}.',
-  }
-
-  # Return the JSON
-  return jsonify(message)
-
 @app.route('/fact', methods=['POST'])
 def fact():
     data = request.get_json()
@@ -60,16 +17,49 @@ def fact():
     
     return jsonify(message)   
 
+@app.route('/insult', methods=['POST'])
+def insult():
+    data = request.get_json()
+    name = data.get('text','').split()[-1]
+    response = requests.get("https://insult.mattbas.org/api/insult").text.split()[2:]
+
+    message = {
+        'author': 'SIT DOWN',
+        'text': f"{name.title()} is {' '.join(response)}."
+    }
+
+    if name.casefold() == "sakthi":
+        message['text'] = "Hi suckthi - hahaha did you get that?"
+    
+    return jsonify(message)   
+
+@app.route('/apologize', methods=['POST'])
+def sorry():
+    data = request.get_json()
+    name = data.get('text','').split()[-1]
+
+    message = {
+        'author': 'IM SORRY',
+        'text': f"{name.title()}, I apologize on behalf of insult bot."
+    }
+
+    if name.casefold() == "sakthi":
+        message['text'] = "Sorry not sorry."
+    
+    return jsonify(message)  
+
+
 @app.route('/weather', methods=['POST'])
 def weather():
     data = request.get_json()
     response = json.loads(requests.get('http://api.openweathermap.org/data/2.5/weather',
                             params = dict(q="Melbourne", APPID=KEY_WEATHER, units="metric")
-                            ).text)
+                            ).text) # this is fixed to Melbourne for now
 
     temp = response["main"]
     attr = response["weather"][0]
 
+    # literally hard-coded oops
     if attr['main'] == "Clear":
         text = f"Today, the weather is {attr['main']} with a maximum temperature of {temp['temp_max']} degrees."
     else:
@@ -81,27 +71,23 @@ def weather():
     }
 
     return jsonify(message)
-  
-@app.route('/wolfram', methods=['POST'])
-def wolfram():
-    data = request.get_json()
-    q = data.get('text','').split()[-1]
-    response = requests.get(f'http://api.wolframalpha.com/v2/query?input={q}&appid={KEY_WOLFRAM}').text
 
-    message = {
-        'author': 'WOLFRAM',
-        'text': response
-    }
-
-    return jsonify(message)
-
-@app.route('/japan_place', methods=['POST'])
+@app.route('/find', methods=['POST'])
 def find():
     data = request.get_json()
     command = data.get('text').split(",")
-    query, loc = ''.join(command[0].split()[1:]), ''.join(command[1])
+    
+    try:
+        query, loc, code = ''.join(command[0].split()[1:]), ''.join(command[1]), command[2]
+    except:
+        message = {
+        'author': 'FINDER',
+        'text': "The usage for this bot is: `find PLACE/QUERY, LOCATION, COUNTRY`."
+        }
+        return message
 
-    URL1 = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={loc},JP&inputtype=textquery&key={API_KEY}"
+    # TODO allow a country to be specified
+    URL1 = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={loc},{code}&inputtype=textquery&key={API_KEY}"
 
     placeid = requests.get(URL1).json()['candidates'][0]['place_id']
     URL2 = f"https://maps.googleapis.com/maps/api/place/details/json?placeid={placeid}&key={API_KEY}"
@@ -113,21 +99,35 @@ def find():
 
     response = requests.get(URL3).json()['candidates'][0]
 
-
     name = response['name']
     rating = response['rating']
     address = []
 
+    # a shit way of shortening the address (instead of including the city and country too)
     for word in response['formatted_address'].split(', '):
-        if not "Tokyo" in word and not "Japan" in word:
+        if not code.casefold() in word.casefold():
             address.append(word)
 
     message = {
-        'author': 'JAP_BOT',
+        'author': 'FINDER',
         'text': f"The closest place is {name} with a {rating} star rating. They are located at {', '.join(address)}."
     }
 
     return jsonify(message)
+
+# Broken!!!
+@app.route('/wolfram', methods=['POST'])
+def wolfram():
+    data = request.get_json()
+    q = data.get('text','').split()[-1]
+    response = requests.get(f'http://api.wolframalpha.com/v2/query?input={q}&appid={KEY_WOLFRAM}').text
+
+    message = {
+        'author': 'WOLFRAM',
+        'text': response
+    }
+
+    return jsonify(message) # output is an xml which is pre broken
 
 
 app.run(host='0.0.0.0')
